6f0fa1c1c391ffba979373082554a39974644682
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2012-11-15 19:34:40 -0800

    lock_unit: add test_thread_contention
    * add test_thread_contention
    
    * test_multi_mutex_lock should destroy the mutexes when it's done with
    them.
    
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- lock_unit.c
+++ lock_unit.c
@@ -34,6 +34,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static int test_multi_mutex_lock(signed int max_locks)
 {
@@ -52,6 +53,79 @@ static int test_multi_mutex_lock(signed int max_locks)
 	for (i = max_locks - 1; i >= 0; i--) {
 		EXPECT_ZERO(lksmith_mutex_unlock(&mutex[i]));
 	}
+	for (i = 0; i < max_locks; i++) {
+		EXPECT_ZERO(lksmith_mutex_destroy(&mutex[i]));
+	}
+	return 0;
+}
+
+struct contention_data {
+	struct lksmith_mutex *locks;
+	int num_locks;
+	int *thread_data;
+	pthread_t *threads;
+	int num_threads;
+};
+
+static struct contention_data cdata;
+
+static int do_test_thread_contention_impl(int idx)
+{
+	int i;
+
+	while (cdata.thread_data[idx]-- > 0) {
+		for (i = 0; i < cdata.num_locks; i++) {
+			EXPECT_ZERO(lksmith_mutex_lock(&cdata.locks[i]));
+			pthread_yield();
+		}
+		for (i = 0; i < cdata.num_locks; i++) {
+			EXPECT_ZERO(lksmith_mutex_unlock(
+				&cdata.locks[cdata.num_locks - i - 1]));
+			pthread_yield();
+		}
+	}
+	return 0;
+}
+
+static void *do_test_thread_contention(void *v)
+{
+	return (void*)(intptr_t)
+		do_test_thread_contention_impl((int)(intptr_t)v);
+}
+
+static int test_thread_contention(int num_locks, int num_threads)
+{
+	int i;
+	char name[LKSMITH_LOCK_NAME_MAX];
+	void *rval;
+
+	memset(&cdata, 0, sizeof(cdata));
+	cdata.locks = xcalloc(sizeof(struct lksmith_mutex) * num_locks);
+	cdata.num_locks = num_locks;
+	cdata.thread_data = xcalloc(sizeof(int) * num_threads);
+	cdata.threads = xcalloc(sizeof(pthread_t) * num_threads);
+	cdata.num_threads = num_threads;
+
+	for (i = 0; i < num_locks; i++) {
+		snprintf(name, sizeof(name), "test_cnt_%04d", i);
+		EXPECT_ZERO(lksmith_mutex_init(name, &cdata.locks[i], NULL));
+		cdata.thread_data[i] = 15;
+	}
+	for (i = 0; i < num_threads; i++) {
+		EXPECT_ZERO(pthread_create(&cdata.threads[i], NULL,
+			do_test_thread_contention, (void*)(intptr_t)i));
+	}
+	for (i = 0; i < num_threads; i++) {
+		pthread_join(cdata.threads[i], &rval);
+		EXPECT_EQ(rval, NULL);
+	}
+	for (i = 0; i < num_locks; i++) {
+		lksmith_mutex_destroy(&cdata.locks[i]);
+	}
+	free(cdata.locks);
+	free(cdata.thread_data);
+	free(cdata.threads);
+
 	return 0;
 }
 
@@ -61,6 +135,8 @@ int main(void)
 
 	EXPECT_ZERO(test_multi_mutex_lock(5));
 	EXPECT_ZERO(test_multi_mutex_lock(100));
-
+	EXPECT_ZERO(test_thread_contention(3, 2));
+	EXPECT_ZERO(test_thread_contention(2, 3));
+	EXPECT_ZERO(test_thread_contention(15, 60));
 	return EXIT_SUCCESS;
 }
