9dffce77c8263008ddfe2b109219e2c0e2b6cc15
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2012-11-18 13:21:51 -0800

    Add util.c/util.h
    Add util.c for fwdprintf
    
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

---
+++ util.c
@@ -0,0 +1,53 @@
+/*
+ * vim: ts=8:sw=8:tw=79:noet
+ *
+ * Copyright (c) 2011-2012, the Locksmith authors.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "util.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+void fwdprintf(char *buf, size_t *off, size_t buf_len, const char *fmt, ...)
+{
+	int res;
+	size_t o;
+	va_list ap;
+
+	o = *off;
+	va_start(ap, fmt);
+	res = vsnprintf(buf + o, buf_len - o, fmt, ap);
+	va_end(ap);
+	if (res < 0)
+		return;
+	else if (o + res > buf_len)
+		*off = buf_len;
+	else
+		*off = o + res;
+}
---
+++ util.h
@@ -0,0 +1,47 @@
+/*
+ * vim: ts=8:sw=8:tw=79:noet
+ *
+ * Copyright (c) 2011-2012, the Locksmith authors.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef LKSMITH_UTIL_H
+#define LKSMITH_UTIL_H
+
+#include <unistd.h> /* for size_t */
+
+/** Write a formatted string to the next available position in a
+ * fixed-length buffer
+ *
+ * @param buf		the buffer
+ * @param off		(inout) current offset in the buffer
+ * @param max		length of the buffer
+ * @param fmt		a printf-style format string
+ * @param ...		format arguments
+ */
+void fwdprintf(char *buf, size_t *off, size_t buf_len,
+	const char *fmt, ...) __attribute__((format(printf, 4, 5)));
+
+#endif
